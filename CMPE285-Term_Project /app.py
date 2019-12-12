5f0faaa62e28901c2e341808dc6eb0233125b0a5
Hyung Yul Choi <55113329+hyungyulchoi@users.noreply.github.com>
2019-12-11 18:55:20 -0800

    Backend changes

--- CMPE285-Term_Project /app.py
+++ CMPE285-Term_Project /app.py
@@ -1,9 +1,9 @@
 from flask import Flask, flash, render_template, request, redirect, url_for, session
+import datetime, pytz, time
+import os.path, json
 from pandas_datareader import data as pdr
 import yfinance as yf
 from sqlalchemy import *
-import datetime, pytz, time
-import os.path, json
 
 # fix yahoo
 yf.pdr_override()
@@ -15,19 +15,19 @@ app.secret_key = b'Stock Portfolio Suggestion Engine'
 # prepare data
 strategies = {
     'e_invest': ['apple', 'adobe', 'nestle'],
-    'g_invest': ['amazon', 'netflix', 'tesla'],
+    'g_invest': ['vertex', 'netflix', 'tesla'],
     'i_invest': ['vanguard', 'ishares1', 'ishares2'],
-    'q_invest': ['general_electric', 'home_depot', 'mcdonalds'],
-    'v_invest': ['johnson', 'blizzard', 'disney']
+    'q_invest': ['general_electric', 'nike', 'disney'],
+    'v_invest': ['magna', 'toll', 'vornando']
 }
 stock_map = {
     'apple': {'name': 'Apple Inc.', 'ticker': 'aapl', 'percent': 25, 'strategy': 'Ethical Investing'},
-    'adobe': {'name': 'Adobe Systems Incorporated', 'ticker': 'adbe', 'percent': 25, 'strategy': 'Ethical Investing'},
-    'nestle': {'name': 'Nestle SA (ADR)', 'ticker': 'nsrgy', 'percent': 50, 'strategy': 'Ethical Investing'},
+    'adobe': {'name': 'Adobe Systems Incorporated', 'ticker': 'adbe', 'percent': 50, 'strategy': 'Ethical Investing'},
+    'nestle': {'name': 'Nestle SA (ADR)', 'ticker': 'nsrgy', 'percent': 25, 'strategy': 'Ethical Investing'},
 
-    'amazon': {'name': 'Amazon.com Inc.', 'ticker': 'amzn', 'percent': 25, 'strategy': 'Growth Investing'},
-    'netflix': {'name': 'Netflix Inc.', 'ticker': 'nflx', 'percent': 25, 'strategy': 'Growth Investing'},
-    'tesla': {'name': 'Tesla Inc.', 'ticker': 'tsla', 'percent': 50, 'strategy': 'Growth Investing'},
+    'vertex': {'name': 'Vertex Pharmaceuticals Incorporated', 'ticker': 'vrtx', 'percent': 30, 'strategy': 'Growth Investing'},
+    'netflix': {'name': 'Netflix Inc.', 'ticker': 'nflx', 'percent': 30, 'strategy': 'Growth Investing'},
+    'tesla': {'name': 'Tesla Inc.', 'ticker': 'tsla', 'percent': 40, 'strategy': 'Growth Investing'},
 
     'vanguard':
         {'name': 'Vanguard Total Stock Market ETF', 'ticker': 'vti', 'percent': 34, 'strategy': 'Index Investing'},
@@ -36,14 +36,13 @@ stock_map = {
     'ishares2':
         {'name': 'iShares Core 10+ Year USD Bond', 'ticker': 'iltb', 'percent': 33, 'strategy': 'Index Investing'},
 
-    'general_electric':
-        {'name': 'General Electric Company', 'ticker': 'ge', 'percent': 40, 'strategy': 'Quality Investing'},
-    'home_depot': {'name': 'Home Depot Inc.', 'ticker': 'hd', 'percent': 20, 'strategy': 'Quality Investing'},
-    'mcdonalds': {'name': 'McDonald\'s Corporation', 'ticker': 'mcd', 'percent': 40, 'strategy': 'Quality Investing'},
+    'general_electric':{'name': 'General Electric Company', 'ticker': 'ge', 'percent': 20, 'strategy': 'Quality Investing'},
+    'nike': {'name': 'Nike Inc.', 'ticker': 'nke', 'percent': 20, 'strategy': 'Quality Investing'},
+    'disney': {'name': 'Walt Disney Co.', 'ticker': 'dis', 'percent': 60, 'strategy': 'Quality Investing'},
 
-    'johnson': {'name': 'Johnson & Johnson', 'ticker': 'jnj', 'percent': 40, 'strategy': 'Value Investing'},
-    'blizzard': {'name': 'Activision Blizzard Inc.', 'ticker': 'atvi', 'percent': 20, 'strategy': 'Value Investing'},
-    'disney': {'name': 'Walt Disney Co.', 'ticker': 'dis', 'percent': 40, 'strategy': 'Value Investing'}
+    'magna': {'name': 'Magna International', 'ticker': 'mga', 'percent': 30, 'strategy': 'Value Investing'},
+    'toll': {'name': 'Toll Brothers', 'ticker': 'tol', 'percent': 30, 'strategy': 'Value Investing'},
+    'vornando': {'name': 'Vornado Realty Trust', 'ticker': 'vno', 'percent': 40, 'strategy': 'Value Investing'}
 }
 
 # set up database
@@ -139,6 +138,7 @@ def process():
         try:
             download_data = pdr.get_data_yahoo(stock_name, start=start_date, end=end_date)
             for row in download_data.itertuples(index=True, name='Pandas'):
+                print(row)
                 data[key].append({
                     # 'date': row.Index,
                     'open': row.Open,
@@ -146,8 +146,9 @@ def process():
                     'low': row.Low,
                     'close': row.Close,
                     'volume': row.Volume,
-                    'adj_close': row._5,
+                    'adj_close': row._1,
                 })
+                print("test4")
         except Exception:
             # May occur a ValueError: zero-size array to reduction operation maximum which has no identity
             _flashes = session.get('_flashes', [])
